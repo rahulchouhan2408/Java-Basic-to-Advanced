f9a018487a0c66f62b52ffd1b8f410f5f0bc4966
Rahul Chouhan <rahulchouhan2408@gmail.com>
2024-11-10 21:32:55 +0530

    Right half pyramid without logic

--- src/BasicProgram/rightHalfPyramid.java
+++ src/BasicProgram/rightHalfPyramid.java
@@ -1,2 +1,12 @@
-package BasicProgram;public class rightHalfPyramid {
+package BasicProgram;
+
+public class rightHalfPyramid {
+    public static void main(String[] args){
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+
+    }
 }
