ac9b0ee67242e2a4e5decadc2b67032cab32903a
Rahul Chouhan <rahulchouhan2408@gmail.com>
2024-11-10 21:28:20 +0530

    Basic Program of Java

---
+++ src/BasicProgram/basic.java
@@ -0,0 +1,7 @@
+package BasicProgram;
+
+public class basic {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
---
+++ src/BasicProgram/leftHalfPyramid.java
@@ -0,0 +1,2 @@
+package BasicProgram;public class leftHalfPyramid {
+}
