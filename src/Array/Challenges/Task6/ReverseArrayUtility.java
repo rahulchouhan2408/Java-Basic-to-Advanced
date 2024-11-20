87d68a2559f98501c4cdf08d10f28c895595ca2b
Rahul Chouhan <rahulchouhan2408@gmail.com>
2024-11-20 13:59:09 +0530

    Utility class of ReverseArray.java

--- src/Array/Challenges/Task6/ReverseArrayUtility.java
+++ src/Array/Challenges/Task6/ReverseArrayUtility.java
@@ -1,2 +1,27 @@
-package Array.Challenges.Task6;public class ReverseArrayUtility {
+package Array.Challenges.Task6;
+
+import java.util.Scanner;
+
+public class ReverseArrayUtility {
+    public static int[] inputArray(){
+        Scanner Arr = new Scanner(System.in);
+        System.out.print("Please enter the number of elements: ");
+        int Size = Arr.nextInt();
+        int[] myArray = new int[Size];
+        int i = 0;
+        while(i<Size){
+        System.out.print("Please enter the element number "+(i+1)+":");
+        myArray[i]=Arr.nextInt();
+            i++;
+        }
+        return myArray;
+    }
+    public static void displayArr(int[] myArray){
+        int i = 0;
+        while(i< myArray.length){
+            System.out.println(myArray[i]+" ");
+            i++;
+        }
+        System.out.println();
+    }
 }
