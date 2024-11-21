9f3e62e14e931ed26dd2ca81f6d26180d6b0be39
Rahul Chouhan <rahulchouhan2408@gmail.com>
2024-11-21 18:07:09 +0530

    Utility class of PalindromeArray.java

--- src/Array/Challenges/Task7/PalindromeArrayUtility.java
+++ src/Array/Challenges/Task7/PalindromeArrayUtility.java
@@ -1,2 +1,19 @@
-package Array.Challenges.Task7;public class PalindromeArrayUtility {
+package Array.Challenges.Task7;
+
+import java.util.Scanner;
+
+public class PalindromeArrayUtility {
+    public static int[] inputArray(){
+        Scanner Arr = new Scanner(System.in);
+        System.out.print("Please enter the number of elements: ");
+        int Size = Arr.nextInt();
+        int[] myArray = new int[Size];
+        int i = 0;
+        while(i<Size){
+            System.out.print("Please enter the element number "+(i+1)+":");
+            myArray[i]=Arr.nextInt();
+            i++;
+        }
+        return myArray;
+    }
 }
